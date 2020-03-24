03a1141f803d9a61222949e31439045aceb2a45e
Devin Abbott <devinabbott@gmail.com>
2020-03-24 14:26:34 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # ThemedColor
 
-A description of this package.
+A backwards-compatible wrapper around  `NSColor`'s new `dynamicProvider` initializer introduced in macOS 10.15: https://developer.apple.com/documentation/appkit/nscolor/3294481-init  
--- Sources/ThemedColor/ThemedColor.swift
+++ Sources/ThemedColor/ThemedColor.swift
@@ -1,3 +1,41 @@
-struct ThemedColor {
-    var text = "Hello, World!"
+import AppKit
+
+extension NSColor {
+    public static func themed(color: @escaping @autoclosure () -> NSColor) -> NSColor {
+        if #available(OSX 10.15, *) {
+            // 10.15 lets us update a color dynamically based on current theme
+            return self.init(name: nil, dynamicProvider: { appearance in
+                return color()
+            })
+        } else {
+            return color()
+        }
+    }
+
+    public static func themed(
+        light lightColor: @escaping @autoclosure () -> NSColor,
+        dark darkColor: @escaping @autoclosure () -> NSColor
+    ) -> NSColor {
+        if #available(OSX 10.15, *) {
+            // 10.15 lets us update a color dynamically based on current theme
+            return self.init(name: nil, dynamicProvider: { appearance in
+                switch appearance.bestMatch(from: [.aqua, .darkAqua]) {
+                case .some(.darkAqua):
+                    return darkColor()
+                default:
+                    return lightColor()
+                }
+            })
+        } else if #available(OSX 10.14, *) {
+            // In 10.14 we can set a color once based on app theme
+            switch NSApp.effectiveAppearance.bestMatch(from: [.aqua, .darkAqua]) {
+            case .some(.darkAqua):
+                return darkColor()
+            default:
+                return lightColor()
+            }
+        } else {
+            return lightColor()
+        }
+    }
 }
